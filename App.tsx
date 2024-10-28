3f1909f070c698156297b8f03a833a2345e37493
youknowme <109729697+youknowme9211@users.noreply.github.com>
2024-10-28 10:10:34 +0530

    Revert "added api"
    This reverts commit b97868e3e810ffd22ba7eacb124c5e5a2dd3278d.

--- App.tsx
+++ App.tsx
@@ -1,16 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import Customicon from './src/components/Customicon';
-import {COLORS} from './src/theme/theme';
 import React from 'react'
-import { upcomingMovies,popularMovies } from "./src/api/apicalls";
 
 const App = () => {
   return (
     <View>
     
       <Text>App</Text>
-
-
     </View>
   )
 }
